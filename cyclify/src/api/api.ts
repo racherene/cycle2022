9ab18fcd8079240bff6b7e6368b703c3a58d5386
dingdongg <cs.dg.kim@gmail.com>
2022-11-19 14:30:54 -0800

    implement getCarEmissions

--- cyclify/src/api/api.ts
+++ cyclify/src/api/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const baseUrlDistance = "";
-const baseUrlCarbon = "https://beta3.api.climatiq.io/estimate";
+const baseUrlCarbon = "https://beta3.api.climatiq.io/custom-activities/estimate";
+const bearerToken = process.env.REACT_APP_CLIMATIQ_API_KEY;
 
 
 const distanceAPI = {
@@ -10,7 +11,22 @@ const distanceAPI = {
 
 const carbonAPI = {
     getCarEmissions: () => {
-        // stub
+        const body = {
+            "custom_activity": {
+                label: "EMISSION_BY_CAR_PER_KM",
+            },
+            "parameters": {
+                distance: 100,
+                distance_unit: "km",
+            },
+        };
+
+        const results = axios.post(baseUrlCarbon, JSON.stringify(body), {
+            headers: {
+                "Authorization": bearerToken,
+            },
+        });
+        return results;
     },
     getBusEmissions: () => {
         // stub
