0b13e485792aed6b87411be64d2bec8bc7b0a655
dingdongg <cs.dg.kim@gmail.com>
2022-11-19 14:40:59 -0800

    create emissions request interface

--- cyclify/src/api/api.ts
+++ cyclify/src/api/api.ts
@@ -4,24 +4,38 @@ const baseUrlDistance = "";
 const baseUrlCarbon = "https://beta3.api.climatiq.io/custom-activities/estimate";
 const bearerToken = process.env.REACT_APP_CLIMATIQ_API_KEY;
 
+interface IEmissionsRequest {
+    custom_activity: {
+        label: string;
+    };
+    parameters: {
+        distance: number;
+        distance_unit: string;
+    };
+}
+
+const createBody = (distance: number, units: string) => {
+    return {
+        custom_activity: {
+            label: "EMISSION_BY_CAR_PER_KM",
+        },
+        parameters: {
+            distance,
+            distance_unit: units,
+        },
+    } as IEmissionsRequest;
+}
+
 
 const distanceAPI = {
 
 };
 
 const carbonAPI = {
-    getCarEmissions: () => {
-        const body = {
-            "custom_activity": {
-                label: "EMISSION_BY_CAR_PER_KM",
-            },
-            "parameters": {
-                distance: 100,
-                distance_unit: "km",
-            },
-        };
+    getCarEmissions: async (distance: number, units: string) => {
+        const body = createBody(distance, units);
 
-        const results = axios.post(baseUrlCarbon, JSON.stringify(body), {
+        const results = await axios.post(baseUrlCarbon, JSON.stringify(body), {
             headers: {
                 "Authorization": bearerToken,
             },
