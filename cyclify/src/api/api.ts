ee5ed2e3f65b0d5b2316f1954bab7e7f9060ad5b
dingdongg <cs.dg.kim@gmail.com>
2022-11-19 13:45:24 -0800

    create basic api handler

---
+++ cyclify/src/api/api.ts
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+const baseUrlDistance = "";
+const baseUrlCarbon = "https://beta3.api.climatiq.io/estimate";
+
+
+const distanceAPI = {
+
+};
+
+const carbonAPI = {
+    getCarEmissions: () => {
+        // stub
+    },
+    getBusEmissions: () => {
+        // stub
+    },
+    getBicycleEmissions: () => {
+        // stub
+    },
+};
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+    carbonAPI,
+    distanceAPI,
+};
