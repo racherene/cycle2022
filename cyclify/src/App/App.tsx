38710ad8b3bca1965553fcad8467885ab86b70cf
dingdongg <cs.dg.kim@gmail.com>
2022-11-19 14:30:41 -0800

    console log API call

--- cyclify/src/App/App.tsx
+++ cyclify/src/App/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import logo from "../logo.svg";
 import '../App.css';
+import api from "../api/api";
 
 function App() {
+  const callAPI = async () => {
+    const results = await api.carbonAPI.getCarEmissions();
+    console.log(results.data);
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -15,6 +20,7 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
+          onClick={callAPI}
         >
           Learn React
         </a>
