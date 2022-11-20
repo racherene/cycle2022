9fb69cde09b96b4e18ad3cc99faf2d1dd4eddef9
dingdongg <cs.dg.kim@gmail.com>
2022-11-20 10:43:08 -0800

    wrap into form element, add submit button

--- cyclify/src/Components/Journey/AddressInput.tsx
+++ cyclify/src/Components/Journey/AddressInput.tsx
@@ -4,12 +4,10 @@ import Dropdown from 'react-bootstrap/Dropdown';
 
 // "Props" will be propogated to parent component (Journey.tsx in this case)
 type Props = {
-    propStartAddress: (newType: string) => void;
-    propEndAddress: (newType: string) => void;
-    propDistance: (newType: number) => void;
+    propInputs: (event: any, start: string, end: string, dist: number) => void;
 };
 
-export default function AddressInput({ propStartAddress, propEndAddress, propDistance }: Props) {
+export default function AddressInput({ propInputs }: Props) {
     // Define state variables
     const [startAddress, setStartAddress] = useState<undefined | string>('');
     const [endAddress, setEndAddress] = useState<undefined | string>('');
@@ -18,49 +16,54 @@ export default function AddressInput({ propStartAddress, propEndAddress, propDis
     // Define handlers for when state changes
     function handleStartAddressChange(event: any) {
         setStartAddress(event.target.value);
-        propStartAddress(event.target.value);
     }
     function handleEndAddressChange(event: any) {
         setEndAddress(event.target.value);
-        propEndAddress(event.target.value);
     }
     function handleDistanceChange(event: any) {
         setDistance(event.target.value);
-        propDistance(event.target.value);
     }
 
     return (
         <div>
             
         <div className="address-input">
-            <div className='center'>
-                <input
-                type="text"
-                id="start-address-input"
-                className="standard-input"
-                placeholder='Location from:'
-                onChange={handleStartAddressChange}
-                />
-                <BsArrowRight style={{fontSize: "50px", margin: "0px 20px"}}/>
-                {/* <img src={BsArrowRight} id="address-arrow" alt="Right Pointing Finger"></img> */}
-                <input
+            <form onSubmit={e => propInputs(
+                e, 
+                startAddress as string, 
+                endAddress as string, 
+                distance as number
+            )}>
+                <div className='center'>
+                    <input
                     type="text"
-                    id="end-address-input"
+                    id="start-address-input"
                     className="standard-input"
-                    placeholder='Location to:'
-                    onChange={handleEndAddressChange}
-                />
-            </div>
-            <div  className='center text text-secondary-dark header'  id="address-seperator">Or</div>
-            <div className='center'>
-                <input
-                    type="number"
-                    id="distance-input"
-                    className="standard-input"
-                    placeholder='Total Distance (KM):'
-                    onChange={handleDistanceChange}
-                />
-            </div>
+                    placeholder='Location from:'
+                    onChange={handleStartAddressChange}
+                    />
+                    <BsArrowRight style={{fontSize: "50px", margin: "0px 20px"}}/>
+                    {/* <img src={BsArrowRight} id="address-arrow" alt="Right Pointing Finger"></img> */}
+                    <input
+                        type="text"
+                        id="end-address-input"
+                        className="standard-input"
+                        placeholder='Location to:'
+                        onChange={handleEndAddressChange}
+                    />
+                </div>
+                <div  className='center text text-secondary-dark header'  id="address-seperator">Or</div>
+                <div className='center'>
+                    <input
+                        type="number"
+                        id="distance-input"
+                        className="standard-input"
+                        placeholder='Total Distance (KM):'
+                        onChange={handleDistanceChange}
+                    />
+                </div>
+                <button type="submit">Determine My Results</button>
+            </form>
             
         </div>
         </div>
