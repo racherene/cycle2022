e57c5ab6cbbda1c601247d397ec006d68a870e44
Matthew Wu <matthew.wu@charmgames.com>
2022-11-19 18:10:12 -0800

    Working implementation for routing

--- cyclify/src/Components/App.tsx
+++ cyclify/src/Components/App.tsx
@@ -1,30 +1,18 @@
 import React from 'react';
-import logo from "../logo.svg";
 import '../App.css';
-import Tester from "./TestComp";
-
-import {BrowserRouter, BrowserRouterProps} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Home from "./Home";
+import Navigation from "./Navigation";
 
 function App() {
     return (
         <div className="App">
             <BrowserRouter>
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <p>
-                        Edit <code>src/App.tsx</code> and save to reload.
-                    </p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
-                </header>
-
-                <Tester></Tester>
+                <Navigation></Navigation>
+                
+                <Routes>
+                    <Route path='/' element={<Home></Home>}></Route>
+                </Routes>
             </BrowserRouter>
         </div>
     );
--- cyclify/src/Components/Home.tsx
+++ cyclify/src/Components/Home.tsx
@@ -1,15 +1,3 @@
-import React from "react";
-import {Link} from 'react-router-dom';
-import image from "../Resources/download.png";
-
-function Home() {
-    return(
-        <div>
-            Home
-            <Link to='/about'>Go to about</Link>
-            <img src={image} alt="Placeholder"></img>
-        </div>
-    )
+export default function Home() {
+    return <h1> Home</h1>
 }
-
-export default Home;
