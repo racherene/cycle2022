d4d2c17f6e8daba6524e0517b7d53260b37e92ae
Matthew Wu <matthew.wu@charmgames.com>
2022-11-20 00:01:06 -0800

    Journey child component is able to pass data to parent

--- cyclify/src/Components/Journey/Journey.tsx
+++ cyclify/src/Components/Journey/Journey.tsx
@@ -1,18 +1,36 @@
-import React from "react";
+import React, {useState} from "react";
 import Map from "../../API/Map/Map";
 import AddressInput from "./AddressInput";
 import ModeInput from "./ModeInput";
 
 export default function Home() {
+    // Define state variables
+    const [startAddress, setStartAddress] = useState<undefined | string>('');
+    const [endAddress, setEndAddress] = useState<undefined | string>('');
+    const [distance, setDistance] = useState<undefined | number>();
+    
+    // Handlers for when state variables change
+    function propStartAddress(address: string) {
+        setStartAddress(address);
+    }
+    function propEndAddress(address: string) {
+        setEndAddress(address);
+    }
+    function propDistance(distance: number) {
+        setDistance(distance);
+    }
+    
     return (
         <div className="journey">
             <div className="background"></div>
             <h2>Create your journey!</h2>
             <div className="input-split">
-                <AddressInput></AddressInput>
-                <Map></Map>
+                <AddressInput propStartAddress={propStartAddress} propEndAddress={propEndAddress} propDistance={propDistance}></AddressInput>
                 <ModeInput></ModeInput>
             </div>
+            <p>{startAddress}</p>
+            <p>{endAddress}</p>
+            <p>{distance}</p>
         </div>
     );
 }
