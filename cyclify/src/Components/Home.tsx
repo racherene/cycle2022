67f49f51276baee11cf7d40ba3719f4422d04c19
racherene <rachelveronikarenegado@gmail.com>
2022-11-19 19:32:19 -0700

    Merge main with Rachel

--- cyclify/src/Components/Home.tsx
+++ cyclify/src/Components/Home.tsx
@@ -1,3 +1,11 @@
+import homeImage from "../Images/HomeImage.svg"
+
 export default function Home() {
-    return <h1> Home</h1>
+    return (
+        <div className="image-wrapper">
+            <img className="homeImage" src={homeImage}></img>
+            <div className="overlay"></div>
+            
+        </div>
+    );
 }
--- cyclify/src/Components/Navigation.tsx
+++ cyclify/src/Components/Navigation.tsx
@@ -16,4 +16,4 @@ export default function Navigation() {
             </div>
         </div>
     );
-}
+} 
